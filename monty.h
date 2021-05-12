bf7b2f2bba9f4fbc52576b5bad1f16b947e7b3cb
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-05-12 10:29:20 +0300

    more function prototypes

--- monty.h
+++ monty.h
@@ -48,6 +48,18 @@ void pop(stack_t **h, unsigned int line_number);
 void pint(stack_t **h, unsigned int line_number);
 void pall(stack_t **h, unsigned int line_number);
 void swap(stack_t **h, unsigned int line_number);
+void nop(stack_t **h, unsigned int line_number);
+void pchar(stack_t **h, unsigned int line_number);
+void pstr(stack_t **h, unsigned int line_number);
+void rotl(stack_t **h, unsigned int line_number);
+void rotr(stack_t **h, unsigned int line_number);
+
+/* calculator prototypes */
+void _add(stack_t **h, unsigned int line_number);
+void _sub(stack_t **h, unsigned int line_number);
+void _mul(stack_t **h, unsigned int line_number);
+void _div(stack_t **h, unsigned int line_number);
+void _mod(stack_t **h, unsigned int line_number);
 
 /* doubly linked list related prototypes */
 int add_end_node(stack_t **h, int n);
