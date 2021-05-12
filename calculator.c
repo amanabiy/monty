6cebdd817ab5cf52ed657d861ae6f9ff1ada29af
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-05-12 21:38:00 +0300

    calculate

--- calculator.c
+++ calculator.c
@@ -22,16 +22,18 @@ void _add(stack_t **h, unsigned int line_number)
  * @line_number: line of command
  */
 void _sub(stack_t **h, unsigned int line_number)
-{
-	if (*h == NULL || (*h)->next == NULL)
+{	int result;
+
+	if (!h || !*h || !((*h)->next))
 	{
-		printf("L%u: can't sub, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		status = EXIT_FAILURE;
+		return;
 	}
-	(*h)->next->n -= (*h)->n;
-	(*h) = (*h)->next;
-	free((*h)->prev);
-	(*h)->prev = NULL;
+
+	result = ((*h)->next->n) - ((*h)->n);
+	pop(h, line_number);/*For top node*/
+	(*h)->n = result;
 }
 /**
  * _mul - multiply second node and top node
